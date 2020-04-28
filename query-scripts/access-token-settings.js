8d47e0bc96ff35cf5d4b5c49debd7836b6aa1b39
Steven Githens <steve@githens.org>
2020-04-27 17:57:06 -0700

    Updating instructions slightly, and error handling

--- query-scripts/access-token-settings.js
+++ query-scripts/access-token-settings.js
@@ -7,6 +7,13 @@
     "prd" is one of the configs from the `configs` hash below.
     The second argument is your `gpiiKey`
 
+    If you are connecting to a dev server without a fully signed cert
+    you may need to use:
+    ```bash
+    NODE_TLS_REJECT_UNAUTHORIZED=0 node access-token-settings.js dev 34523-adf23-asdfa3454
+    ```
+    Don't use this for production data.
+
  */
 
 var axios = require("axios"),
@@ -15,6 +22,12 @@ var axios = require("axios"),
 
 var configs = {
     // An example for dev clusters. Change sgithens to your gcp name
+    localhost: {
+        serverUrl: "http://localhost:8081",
+        clientId: "pilot-computer",
+        clientSecret: "pilot-computer-secret",
+        username: "alice"
+    },
     dev: {
         serverUrl: "https://flowmanager.sgithens.dev.gcp.gpii.net",
         clientId: "pilot-computer",
@@ -106,7 +119,7 @@ function getSettings (configInfo) {
         promTogo.resolve(res.data.preferences);
         // console.log(JSON.stringify(res.data.preferences, null, 4));
     }, function (err) {
-        promTogo.resolve(err);
+        promTogo.reject(err);
     });
 
     return promTogo;
@@ -138,8 +151,15 @@ function main() {
     var config = fluid.copy(configs[args[0]]);
     config.username = args[1];
     getAccessTokenAndSettings(config).then(function (data) {
+    // getAccessToken(config).then(function (data) {
         console.log(JSON.stringify(data, null, 4))
+    }, function (err) {
+        console.log("Error: ", err);
     });
 }
 
 main();
+
+// console.log(encodeURI(JSON.stringify(win32device)));
+
+// %7B%22OS%22:%7B%22id%22:%22win32%22%7D,%22solutions%22:%5B%7B%22id%22:%22com.freedomscientific.magic%22%7D,%7B%22id%22:%22com.microsoft.windows.brightness%22%7D,%7B%22id%22:%22com.microsoft.windows.desktopBackground%22%7D,%7B%22id%22:%22com.freedomscientific.jaws%22%7D,%7B%22id%22:%22com.microsoft.windows.desktopBackgroundColor%22%7D,%7B%22id%22:%22com.microsoft.windows.mirrorScreen%22%7D,%7B%22id%22:%22com.microsoft.windows.cursors%22%7D,%7B%22id%22:%22com.microsoft.windows.filterKeys%22%7D,%7B%22id%22:%22com.microsoft.windows.highContrast%22%7D,%7B%22id%22:%22com.microsoft.windows.audioDescription%22%7D,%7B%22id%22:%22com.microsoft.windows.notificationDuration%22%7D,%7B%22id%22:%22com.microsoft.windows.language%22%7D,%7B%22id%22:%22com.microsoft.windows.toggleKeys%22%7D,%7B%22id%22:%22com.microsoft.windows.underlineMenuShortcuts%22%7D,%7B%22id%22:%22com.microsoft.windows.shortcutWarningMessage%22%7D,%7B%22id%22:%22com.microsoft.windows.shortcutWarningSound%22%7D,%7B%22id%22:%22com.microsoft.windows.magnifier%22%7D,%7B%22id%22:%22com.microsoft.windows.mouseKeys%22%7D,%7B%22id%22:%22com.microsoft.windows.mouseSettings%22%7D,%7B%22id%22:%22com.microsoft.windows.mouseTrailing%22%7D,%7B%22id%22:%22com.microsoft.windows.narrator%22%7D,%7B%22id%22:%22com.microsoft.windows.nightScreen%22%7D,%7B%22id%22:%22com.microsoft.windows.soundSentry%22%7D,%7B%22id%22:%22com.microsoft.windows.onscreenKeyboard%22%7D,%7B%22id%22:%22com.microsoft.windows.screenDPI%22%7D,%7B%22id%22:%22com.microsoft.windows.screenResolution%22%7D,%7B%22id%22:%22com.microsoft.windows.stickyKeys%22%7D,%7B%22id%22:%22com.microsoft.windows.touchPadSettings%22%7D,%7B%22id%22:%22com.microsoft.windows.typingEnhancement%22%7D,%7B%22id%22:%22com.microsoft.windows.volumeControl%22%7D,%7B%22id%22:%22com.office.windowsOneNoteLearningTools%22%7D,%7B%22id%22:%22com.office.windowsWordHome365LearningTools%22%7D,%7B%22id%22:%22com.office.windowsWordPro365LearningTools%22%7D,%7B%22id%22:%22com.texthelp.readWriteGold%22%7D,%7B%22id%22:%22net.gpii.test.speechControl%22%7D,%7B%22id%22:%22net.gpii.explode%22%7D,%7B%22id%22:%22net.gpii.uioPlus%22%7D,%7B%22id%22:%22org.nvda-project%22%7D,%7B%22id%22:%22com.microsoft.office%22%7D%5D%7D
